d35a99e651c56698a0e63b885e791a2938ff3f03
techstag <99538584+techstag@users.noreply.github.com>
2022-03-15 10:51:22 +0530

    Update def.js

--- def.js
+++ def.js
@@ -2,6 +2,6 @@ const telegrambtn = document.querySelector('#telegrambtn');
 const telegramtitle = telegrambtn.getAttribute('data-title');
 const telegramusername = telegrambtn.getAttribute('data-uid');
 
-window.addEventListener('load', () => {
+document.addEventListener("DOMContentLoaded", () => {
     telegrambtn.innerHTML = `<div><a href="https://telegram.me/${telegramusername.value}" style="text-decoration:none; background-color: #0088cc; border-radius: 5px; color:#fff;  padding: 5px 10px; font-size: 15px" target="blank">Join ${telegramtitle.value} on Telegram</a></div>`
 });
